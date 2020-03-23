316066d34267692f744ce678749446586dcfcc68
Alexandracope <caplexo1997@gmail.com>
2020-03-23 16:43:22 +0000

    update

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 
-FLASK_APP=app:name
+FLASK_APP=app:SOFIA-Project1
 
 
 app.config['SQLALCHEMY_DATABASE_URI']=getenv('DATABASE_URI')
